440f4d2a8555791d84401bf6228ef979e9001661
liyiwei <13584829170@139.com>
2020-09-25 10:46:27 +0800

    loading优化~

--- generators/app/templates/src/api/http.js
+++ generators/app/templates/src/api/http.js
@@ -1,20 +1,45 @@
 import Axios from 'axios';
+import { Spin, Message } from 'view-design'
 
 Axios.defaults.headers.post['Content-Type'] = 'application/json';
+Axios.defaults.timeout = 5000
+let loadingTimer; //! 加载timer 为了避免：请求返回值很快时出现的loading闪一下
 // 拦截器
 Axios.interceptors.request.use((config) => {
+  if (!loadingTimer) {
+    loadingTimer = setTimeout(() => {
+      Spin.show();
+    }, 400);
+  }
   const reqUrl = config.url
-// 若请求地址不是signIn 则携带token
-  if(reqUrl.indexOf('signIn')==-1){
+  // 若请求地址不是signIn 则携带token
+  if (reqUrl.indexOf('signIn') == -1) {
     config.headers.Authorization = localStorage.riskSys;
   }
   return config;
 }, ((err) => {
   return Promise.reject(err);
 }));
-
+Axios.interceptors.response.use(response => {
+  clearInterval(loadingTimer)
+  Spin.hide();
+  const data = response.data;
+  const { code, error } = data
+  if (code !== "0") {
+    Message.error({
+      content: error.message,
+      duration: 5
+    })
+  }
+  return response
+}, (err) => {
+  Spin.hide();
+  Message.error(err.message)
+  return Promise.reject(err)
+}
+)
 export default {
-  get(url,params,responseType) {
+  get(url, params, responseType) {
     return new Promise((resolve, reject) => {
       Axios({
         method: 'get',
@@ -28,9 +53,7 @@ export default {
         .catch((err) => {
           reject(err);
         });
-    }).catch(err=>{
-      alert(err.message)
-    });
+    })
 
   },
   post(url, data) {
@@ -46,8 +69,6 @@ export default {
         .catch((err) => {
           reject(err);
         });
-    }).catch(err=>{
-      alert(err.message)
-    });
+    })
   },
 };
